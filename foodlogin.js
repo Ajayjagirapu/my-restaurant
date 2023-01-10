4ca1db48b8b5c0e1a92b8c1da3c2c3b0239eecd6
Ajayjagirapu <ajayaj0692@gmail.com>
2023-01-10 15:01:29 +0530

    food sample app

---
+++ foodlogin.js
@@ -0,0 +1,7 @@
+function myFunc(){
+    if(document.getElementById("u1").value=="Ajay" && document.getElementById("p1").value=="minnu23")
+        { window.location.href = "items.html";}
+    else{
+        window.location.href="Error.html";
+    }
+}
